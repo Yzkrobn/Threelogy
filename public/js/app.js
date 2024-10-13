5a66b9055d7ef685050aa405ebd4f4afb06aea70
Yzkrobn <oktavianbagasnugroho@gmail.com>
2024-10-13 08:22:15 -0700

    Memperbarui desain hal 2

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,52 @@
 document.addEventListener('DOMContentLoaded', () => {
     const menuButton = document.getElementById('menuButton');
-    const dropdownnav = document.getElementById('dropDown');
+    const dropDownNav = document.getElementById('dropDown');
+    const container1 = document.getElementById('container-1');
+    const container2 = document.getElementById('container-2');
+    const divImage = document.getElementById('divImage');
 
-    if (menuButton && dropdownnav) {
+    if (menuButton && dropDownNav) {
         menuButton.addEventListener('click', () => {
-            dropDown.classList.toggle('hidden');
+            dropDownNav.classList.toggle('hidden');
 
-            if (window.innerWidth >= 768 && !dropDown.classList.contains('hidden')) {
-                dropDown.classList.remove('absolute', 'top-full', 'translate-y-2', 'pl-2');
+            if (window.innerWidth >= 768 && !dropDownNav.classList.contains('hidden')) {
+                dropDownNav.classList.remove('absolute', 'top-full', 'translate-y-2', 'pl-2');
             } else {
-                dropDown.classList.add('absolute', 'top-full', 'translate-y-2', 'pl-2');
+                dropDownNav.classList.add('absolute', 'top-full', 'translate-y-2', 'pl-2');
             }
         });
     }
+
+    if (divImage) {
+        if (window.innerWidth >= 768) {
+            container2.removeChild(divImage);
+            container2.appendChild(divImage);
+        }else{
+            
+        }
+        
+    }
+
+    const containers = [container1, container2];
+    containers.forEach(flexContainer => {
+        if (flexContainer) {
+            if (window.innerWidth >= 768) {
+                flexContainer.classList.remove('flex-col');
+            } else {
+                flexContainer.classList.add('flex-col');
+            }
+        }
+    });
+
+    window.addEventListener('resize', () => {
+        containers.forEach(flexContainer => {
+            if (flexContainer) {
+                if (window.innerWidth >= 768) {
+                    flexContainer.classList.remove('flex-col');
+                } else {
+                    flexContainer.classList.add('flex-col');
+                }
+            }
+        });
+    });
 });
